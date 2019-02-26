2ed1e08536b0fb8fee5d4fbd4b6e4cafccd6cbb6
Eswsx <957140834@qq.com>
2019-02-26 14:33:27 +0800

    0226

--- introduction_to_Algorithms/src/algorithm/InsertSort.java
+++ introduction_to_Algorithms/src/algorithm/InsertSort.java
@@ -2,15 +2,13 @@ package algorithm;
 
 public class InsertSort {
 	public static void main(String[] args) {
-		double[] MyList= new double[]{5,2,4,6,1,3}; 
+		double[] MyList= new double[]{5,2,4,-1,6,1,3,4,3,4,6,7}; 
 		sort(MyList);
-		for(int i=0;i<6;i++){
-			System.out.println(MyList[i]);
-			}
+		print(MyList);
 		}
 	
 	public static void sort(double[] array){
-		for(int i=0;i<6;i++){
+		for(int i=0;i<array.length;i++){
 			for(int j=0;j<i;j++){
 				if(array[i]<array[j]){
 					double c = 0;
@@ -19,6 +17,12 @@ public class InsertSort {
 					array[j] = c;
 				}
 			}
+		}
+	}
+
+	public static void print(double[] MyList){
+	for(int i=0;i<MyList.length;i++){
+		System.out.println(MyList[i]);
 		}
 	}
 }
